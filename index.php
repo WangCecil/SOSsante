fdc2c9786b378b5e65443536f4727fb986f8c3c3
WangCecil <ceciliawangue@yahoo.fr>
2018-11-29 16:30:35 +0100

    SOSsante premier commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>AlloDocteur</title>
+	<script src="https://www.gstatic.com/firebasejs/5.5.9/firebase.js"></script>
+	<script>
+	  // Initialize Firebase
+	  var config = {
+	    apiKey: "AIzaSyDYQyxP_a982IuPd6AkyfGpf14prYesaiw",
+	    authDomain: "allodocteur-ee75e.firebaseapp.com",
+	    databaseURL: "https://allodocteur-ee75e.firebaseio.com",
+	    projectId: "allodocteur-ee75e",
+	    storageBucket: "allodocteur-ee75e.appspot.com",
+	    messagingSenderId: "129928791994"
+	  };
+	  firebase.initializeApp(config);
+	</script>
+</head>
+<body>
+	<h1>Hello World !</h1>
+</body>
+</html>
